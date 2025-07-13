42a1839a794c29b11c7653d9841ce17b3257cece
Akshay <akshay.dhuri@motilaloswal.com>
2025-07-13 21:08:45 +0530

    How to add fonts

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,12 +1,16 @@
 import "../globals.css";
 import Navigation from "@/components/Navigation";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  subsets: ["latin"],
+});
 export default function RootLayout({children}) {
   return (
     <html lang="en">
       <head>
         <title>Next.js App</title>
       </head>
-      <body>
+      <body className={roboto.className}>
         <Navigation />
         {children}
       </body>
